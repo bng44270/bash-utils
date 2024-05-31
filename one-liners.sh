0a3914988f082f7b75ada745d3aefbea027e605f
Andy Carlson <bng44270@gmail.com>
2024-05-31 11:16:50 -0500

    Update one-liners.sh

--- one-liners.sh
+++ one-liners.sh
@@ -1,4 +1,4 @@
-# md2html (does bold, italic, h1, h2, h3, strikethrough, highlight, and horizontal line)
+# md2html (does newlines, bold, italic, h1, h2, h3, strikethrough, highlight, and horizontal line)
 md2html() {
 	sed 's/  $/<br\/>/g;s/^###[ \t]*\(.*\)$/<h3>\1<\/h3>/g;s/^##[ \t]*\(.*\)$/<h2>\1<\/h2>/g;s/^#[ \t]*\(.*\)$/<h1>\1<\/h1>/g;s/[*]\{2\}\([^*]\+\)[*]\{2\}/<b>\1<\/b>/g;s/[*]\([^*]\+\)[*]/<i>\1<\/i>/g;s/[~]\{2\}\([^~]\+\)[~]\{2\}/<s>\1<\/s>/g;s/[=]\{2\}\([^=]\+\)[=]\{2\}/<mark>\1<\/mark>/g;s/---/<hr\/>/g'
 }
